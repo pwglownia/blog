245e6bfbef5fe66170a27566007887fa9531f7fa
Piotr Głownia <pglownia@semanticore.pl>
2023-07-10 14:42:22 +0200

    feat: add constants, add page meta

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const PAGE_TITLE = "Piotr Głownia | Web Dev's stream of thought";
+export const PAGE_DESCRIPTION = "Personal blog where I publish about web programming. Mainly about frontend, devops, algorithms"
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { PAGE_DESCRIPTION, PAGE_TITLE } from '../constants';
+</script>
+
+<svelte:head>
+	<title>{PAGE_TITLE}</title>
+	<meta name="description" content={PAGE_DESCRIPTION} />
+</svelte:head>
+
+<slot />
