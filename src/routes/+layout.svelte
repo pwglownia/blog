adc3b9ab5bd1a5a6438b6c8080e1e3a56092e17a
Piotr Głownia <pglownia@semanticore.pl>
2023-07-11 00:23:30 +0200

    feat: add global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 	<meta name="description" content={PAGE_DESCRIPTION} />
 </svelte:head>
 
-<div>
+<div class="layout">
 	<Header />
 	<main>
 		<slot />
@@ -22,4 +22,22 @@
 </div>
 
 <style>
+	.layout {
+		height: 100%;
+		max-inline-size: 1440px;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		margin-inline: auto;
+		padding-inline: var(--size-7);
+	}
+
+	main {
+		padding-block: var(--size-9);
+	}
+
+	@media (min-width: 1440px) {
+		.layout {
+			padding-inline: 0;
+		}
+	}
 </style>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -5,3 +5,14 @@
 <footer class="p-4 bg-slate-300">
 	<p>{PAGE_TITLE} &copy {new Date().getFullYear()}</p>
 </footer>
+
+<style>
+	footer {
+		padding-block: var(--size-7);
+		border-top: 1px solid var(--border);
+	}
+
+	p {
+		color: var(--text-2);
+	}
+</style>
