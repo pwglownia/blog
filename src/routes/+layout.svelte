440aaf059f699de5ccc0a43ead3eece576b3532b
Piotr Głownia <pglownia@semanticore.pl>
2023-07-10 14:55:27 +0200

    feat: create header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { PAGE_DESCRIPTION, PAGE_TITLE } from '../constants';
+	import Header from './Header.svelte';
+	import '../app.css';
 </script>
 
 <svelte:head>
 	<title>{PAGE_TITLE}</title>
 	<meta name="description" content={PAGE_DESCRIPTION} />
 </svelte:head>
+
+<Header />
 
 <slot />
---
+++ src/routes/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<nav>
+	<a href="/">blog-title-here</a>
+
+	<ul class="grid">
+		<li><a href="/about">about</a></li>
+		<li><a href="/categories">categories</a></li>
+		<li><a href="/hire-me">hire me</a></li>
+		<li><a href="/rss.xml" target="_blank">RSS</a></li>
+	</ul>
+
+	<button />
+</nav>
